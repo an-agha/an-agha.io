abf8f5d439120bde556eb8e177574ea3f23afaf2
anaghanisakar <anaghanisakar@gmail.com>
2025-08-21 16:08:22 +0530

    benefits section

--- src/components/landing/Benefits.tsx
+++ src/components/landing/Benefits.tsx
@@ -4,12 +4,57 @@ import React from 'react'
 
 function Benefits() {
 
- const list = [
-    'Improvement in sustainability ratings and certifications',
-    'Access to premium green financing and ESG investment opportunities',
-    'Enhanced property values through verified sustainability credentials',
-    'Continuous carbon credit revenue generation throughout building lifecycle',
-    'Market leadership positioning in sustainable development'
+  const benefits = [
+    {
+      image: '/images/benefits/developement.png',
+      title: 'Project Developers & Owners - Strategic Empowerment',
+      list: [
+        'Improvement in sustainability ratings and certifications',
+        'Access to premium green financing and ESG investment opportunities',
+        'Enhanced property values through verified sustainability credentials',
+        'Continuous carbon credit revenue generation throughout building lifecycle',
+        'Market leadership positioning in sustainable development'
+      ]
+    },
+    {
+      image: '/images/benefits/construction.png',
+      title: 'Construction Companies & Contractors - Operational Empowerment',
+      list: [
+        'Integrated sustainability planning from design through completion',
+        'New revenue streams through carbon credit monetization',
+        'Competitive differentiation in sustainable construction market',
+        'Automated compliance reducing administrative burden',
+      ]
+    },
+    {
+      image: '/images/benefits/material-supplier.png',
+      title: 'Material Suppliers & Manufacturers - Market Empowerment',
+      list: [
+        'Platform for showcasing verified sustainability credentials',
+        'Direct integration with high-value sustainable construction projects',
+        'Premium pricing opportunities for verified sustainable materials',
+        'Access to recycling marketplace opportunities',
+      ]
+    },
+    {
+      image: '/images/benefits/waste-management.png',
+      title: 'Waste Management & Recycling - Value Empowerment',
+      list: [
+        'Technology-enabled recycling monitoring',
+        'Enhanced value extraction from waste streams',
+        'Carbon credit revenue sharing opportunities',
+        'Direct connection to construction demand pipeline',
+      ]
+    },
+     {
+      image: '/images/benefits/facility-management.png',
+      title: 'Facility Managers & Operators - Performance Empowerment',
+      list: [
+        'Continuous sustainability monitoring and optimization',
+        'Automated building certification renewal processes',
+        'Long-term carbon credit generation from operations',
+      ]
+    },
   ]
 
   return (
@@ -22,36 +67,42 @@ function Benefits() {
           </span>
           Benefits
         </h2>
-        <div className='flex flex-col items-center md:flex-row md:justify-between gap-10'>
-          <Image
-            src='/images/benefits.png'
-            width={450}
-            height={450}
-            alt="benefits section image"
-            className='w-[300px] lg:w-[450px]'
-          />
-          <Image
-          src="/images/stick.svg"
-          width={2}
-          height={200}
-          alt='decorative image' 
-          className='hidden md:block'
-          />
-          <div className='w-full md:w-max max-w-[670px] bg-primary-foreground flex flex-col gap-6 py-6 px-4 rounded-[20px]'>
-            <h4 className='text-lg lg:text-2xl font-medium text-primary-textclr'>
-              Project Developers & Owners - Strategic Empowerment
-            </h4>
-            <ul className='flex flex-col gap-3'>
-              {list?.map((item, index) => (
-                <li className='flex items-center gap-4' key={index}>
-                  <Icon icon='streamline:graph-solid' color="#10B981" className="text-xl" />
-                  <span className='text-[#3C4B42]'>
-                    {item}
-                  </span>
-                </li>
-              ))}
-            </ul>
-          </div>
+        <div className='w-full flex gap-10 overflow-scroll'>
+          {
+            benefits.map((item, index) => (
+              <div className='min-w-full flex flex-col items-center md:flex-row md:justify-between gap-10'>
+                <Image
+                  src={item.image}
+                  width={450}
+                  height={450}
+                  alt="benefits section image"
+                  className='w-[300px] lg:w-[450px]'
+                />
+                <Image
+                  src="/images/stick.svg"
+                  width={2}
+                  height={200}
+                  alt='decorative image'
+                  className='hidden md:block'
+                />
+                <div className='w-full md:w-max max-w-[670px] bg-primary-foreground flex flex-col gap-6 py-6 px-4 rounded-[20px]'>
+                  <h4 className='text-lg lg:text-2xl font-medium text-primary-textclr'>
+                    {item.title}
+                  </h4>
+                  <ul className='flex flex-col gap-3'>
+                    {item.list?.map((listItem, index) => (
+                      <li className='flex items-center gap-4' key={index}>
+                        <Icon icon='streamline:graph-solid' color="#10B981" className="text-xl" />
+                        <span className='text-[#3C4B42]'>
+                          {listItem}
+                        </span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            ))
+          }
         </div>
       </div>
     </section>
